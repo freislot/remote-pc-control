8614c5860ab2120e108a25480a6409706af1b69d
pantonov <freislot@gmail.com>
2020-02-27 17:46:26 +0300

    Реализовано удаленное управление монитором

--- server.js
+++ server.js
@@ -1,12 +1,15 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const cors = require('cors')
 const win = require('win-audio');
+const displaycontrol = require("display-control");
 
 const app = express();
 const speaker = win.speaker;
 
 app.use(express.static('public'));
-app.use(bodyParser.json())
+app.use(bodyParser.json());
+app.use(cors());
 
 app.get('/get', function (req, res) {
   res.send({ value: speaker.get() });
@@ -34,6 +37,18 @@ app.post('/toggle', (req, res) => {
     value: speaker.get(), 
     isMuted: speaker.isMuted()
   });
+});
+
+app.post('/displayoff', function (req, res) {
+  displaycontrol.sleep();
+  res.sendStatus(200);
+});
+
+app.post('/displayon', function (req, res) {
+  let timerId = setInterval(() => displaycontrol.wake(), 100);
+  setTimeout(() => { clearInterval(timerId); }, 10000);
+
+  res.sendStatus(200);
 });
 
 app.listen(3000, function () {
